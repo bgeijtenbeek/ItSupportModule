5d5846669e545bc43dfb5886cfa8f5cfdae3dc94
bgeijtenbeek <91257517+bgeijtenbeek@users.noreply.github.com>
2025-03-11 09:44:04 +0100

    Initial commit
    Basic upload, nothing fancy yet

---
+++ ItSupportModule/ItSupportModule.psd1
@@ -0,0 +1,45 @@
+﻿# Module manifest for module 'MyUtilities'
+
+@{
+    # Script module or binary module file associated with this manifest
+    RootModule = 'ItSupportModule.psm1'
+
+    # Version number of this module
+    ModuleVersion = '0.0.1'
+
+    # Supported PowerShell version
+    PowerShellVersion = '5.1'
+
+    # Name of the author
+    Author = 'Bastiaan Geijtenbeek'
+
+    # Company or vendor of the module
+    CompanyName = 'GeijtenbeekIT'
+
+    # Description of the module
+    Description = 'A utility module with useful functions for desktop file management'
+
+    # Functions to export from this module
+    FunctionsToExport = @('Start-CustomTranscript', 'Stop-CustomTranscriptSuccess', 'Stop-CustomTranscriptError', 'Write-ToLog')
+
+    # Cmdlets to export (if using binary modules, leave blank for now)
+    CmdletsToExport = @()
+
+    # Variables to export
+    VariablesToExport = @()
+
+    # Aliases to export
+    AliasesToExport = @()
+
+    # List of required modules
+    RequiredModules = @()
+
+    # Private data (useful for additional metadata)
+    PrivateData = @{
+        PSData = @{
+            Tags = @('Utilities', 'Desktop', 'PowerShell', 'Scripting', 'Installation', 'Intune')
+            LicenseUri = 'https://opensource.org/licenses/MIT'
+            ProjectUri = 'https://github.com/bgeijtenbeek/ItSupportModule'
+        }
+    }
+}
---
+++ ItSupportModule/ItSupportModule.psm1
@@ -0,0 +1,109 @@
+﻿##################################################################################################
+# Function to start a transcript with custom naming and path parameters
+##################################################################################################
+function Start-CustomTranscript {
+
+    param (
+        [string]$scriptName
+    )
+
+    #Check whether the script is running in admin or user context
+    $windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $windowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($windowsIdentity)
+    $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+    #Define custom scriptlog folder based on admin or user context and set scriptLogFolder accordingly
+    if ($windowsPrincipal.IsInRole($adminRole)) {
+        $scriptLogFolder = "$env:ProgramData\MWP\ScriptLogs\$scriptName"
+    } else {
+        $scriptLogFolder = "$env:LOCALAPPDATA\MWP\ScriptLogs\$scriptName"
+    }
+    
+    #Create logfolder if it doesn't exist yet
+    $scriptLogPathCreated = $false
+    if (!(Test-Path $scriptLogFolder)) {
+        New-Item -ItemType Directory -Path $scriptLogFolder -Force | Out-Null
+        $scriptLogPathCreated = $true
+    }
+
+    #Define variables used in the custom transcript
+    $dateStamp = Get-Date -Format "yyyyMMdd_HHmm"
+    $logName = $scriptName + "_$dateStamp"
+    $fullLogPath = Join-Path -Path $scriptLogFolder -ChildPath "$logname.log"
+
+    #Start the custom named transcript
+    Start-Transcript -Path "$fullLogPath" -Force -ErrorAction SilentlyContinue
+
+    # Write log for creating log folder when it was just created.
+    if ($scriptLogPathCreated) {
+        Write-ToLog -info "Created logfolder $scriptLogFolder before starting the transcript because it did not exist yet."
+    }
+}
+
+##################################################################################################
+# Function to stop the custom transcript with a success message and exit code
+##################################################################################################
+function Stop-CustomTranscriptSuccess {
+    Write-ToLog -success "Everything seems in order, script ran without any noticable issues."
+    Write-Host "Exiting script.."
+    Stop-Transcript
+    Start-Sleep -Seconds 1
+    Exit 0
+}
+
+##################################################################################################
+# Function to stop the custom transcript with an error message and exit code
+##################################################################################################
+function Stop-CustomTranscriptError {
+    Write-ToLog -failure "Something went wrong. Check the logs for further details or manually check the results on the device!"
+    Write-Host "Exiting script.."
+    Stop-Transcript
+    Start-Sleep -Seconds 1
+    Exit 1
+}
+
+##################################################################################################
+# Function to write messages to host with custom formatting
+##################################################################################################
+function Write-ToLog {
+
+    param (
+        [switch]$info,
+        [switch]$warning,
+        [switch]$failure,
+        [switch]$success,
+        [Parameter(Position = 0, ValueFromRemainingArguments = $true)]
+        [string]$CustomLogEntry
+    )
+
+    if ((-not $info.IsPresent) -and (-not $warning.IsPresent) -and (-not $failure.IsPresent) -and (-not $success.IsPresent)){
+        Write-Host "$CustomLogEntry"    
+    }
+    if($info.IsPresent){
+        Write-Host "ℹ️ - $CustomLogEntry" -ForegroundColor Cyan   
+    }
+    if($warning.IsPresent){
+        Write-Host "⚠️ - $CustomLogEntry"-ForegroundColor Yellow       
+    }
+    if($failure.IsPresent){
+        Write-Host "❌ - $CustomLogEntry" -ForegroundColor Red        
+    }
+    if($success.IsPresent){
+        Write-Host "✅ - $CustomLogEntry" -ForegroundColor Green        
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Export the functions so they are available when the module is imported
+Export-ModuleMember -Function Start-CustomTranscript, Stop-CustomTranscriptSuccess, Stop-CustomTranscriptError, Write-ToLog
